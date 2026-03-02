fde5c4eabfd629e231f098c2e592e8d94f886e02
Purple Listening <Sewing the Sky>
2026-03-02 08:54:21 +0800

    git feat -m "IDT takes effect, handler function hlt."
    - Prerequisite: The C language kernel actively adds a page fault description.
    
    - Effect: The program repeatedly restarts.
    
    - Result: No restart description page fault was caught (IDT takes effect), but the handler function is just hlt, so it halts.

--- desolate/headquarters/build.sh
+++ desolate/headquarters/build.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-nasm -f bin boot.asm -o boot.bin
-gcc -m32 -ffreestanding -c kernel.c -o kernel.o
-ld -m elf_i386 -Ttext 0x1000 --oformat binary -o kernel.bin kernel.o
+nasm -f bin boot.asm -o boot.bin                # 编译引导程序
+nasm -f elf32 kernel_start.asm -o kernel_start.o # 编译内核入口汇编
+gcc -m32 -ffreestanding -c kernel.c -o kernel.o  # 编译C内核
+ld -m elf_i386 -Ttext 0x1000 --oformat binary -o kernel.bin kernel_start.o kernel.o  # 链接成内核二进制
 dd if=/dev/zero of=os.img bs=512 count=100
 dd if=boot.bin of=os.img bs=512 count=1 conv=notrunc
 dd if=kernel.bin of=os.img bs=512 seek=1 conv=notrunc
--- desolate/headquarters/kernel.c
+++ desolate/headquarters/kernel.c
@@ -4,5 +4,7 @@ void kernel_main() {
     video_memory[1] = 0x0e;  // 黄色
     video_memory[2] = 'i';
     video_memory[3] = 0x0e;
+    char *kernrl_code = (char*) 0x1000;
+    *kernrl_code = 0x90;
     while(1);
 }
